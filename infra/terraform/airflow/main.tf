267323a2adcf2cea1b28e088ecff624e9aef8012
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-16 20:53:08 -0300

    build: terraform infra + airflow dags + databricks jobs

--- infra/terraform/airflow/main.tf
+++ infra/terraform/airflow/main.tf
@@ -164,18 +164,3 @@ resource "aws_ecs_service" "airflow" {
     aws_db_instance.airflow
   ]
 }
-
-# -------------------------------
-# OUTPUTS
-# -------------------------------
-output "airflow_rds_endpoint" {
-  value = aws_db_instance.airflow.address
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.airflow.name
-}
-
-output "airflow_service_name" {
-  value = aws_ecs_service.airflow.name
-}
